07beb4601a16ec24003406963742e1401cb3c469
NazifaSadia <nazifas323@gmail.com>
2020-06-27 10:44:09 +0600

    Fully Responsive Website

---
+++ README.md
@@ -0,0 +1,1 @@
+# Responsive-Complete-Web-Layout---2
---
+++ index.js
@@ -0,0 +1,38 @@
+var typed = new Typed('#typed',{
+    strings: [
+        'Front End Designer',
+        'Web Developer',
+        'YouTuber'
+    ],
+    typeSpeed:80,
+    typeSpeed:80,
+    typeSpeed:80,
+    loop:true
+});
+
+
+// $(document).ready(function(e){
+//     $win = $(window);
+//     $navbar = ('#header');
+//     $toggle = ('.tiggle-button')
+//     var width = $navbar.width();
+//     tiggle_onclick($win,$navbar,width);
+
+//     // resize evevnt
+//     $win.resize(function(){
+//     tiggle_onclick($win,$navbar,$width);
+//     });
+
+//     $tiggle.click(function(e){
+//         $navbar.tiggleClass("tiggle-left");
+// })
+
+// });
+
+// function tiggle_onclick ($win,$navbar,width) {
+//     if ($win.width()<= 768) {
+//         $navbar.css({left:`-${width}px`});
+//         }else{ 
+//             $navbar.css({left:'0px'});
+// }}
+
